28d44c1b514e10df2aa193372a51a001195e9e2a
Kelvaro <blizzardkang@gmail.com>
2016-01-08 13:15:11 -0800

    Changed the echo into something else

--- index.php
+++ index.php
@@ -5,5 +5,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-echo 'hello world';
+$temp = 'Jim';
+echo 'Hi, my name is';
+echo $temp;
+$temp ='geek';
+echo "I am a";
+echo $temp;
+$temp = 10;
+echo 'My level is';
+echo $temp;
 ?>
