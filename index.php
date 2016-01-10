3241d7847c4d7b29655126ab0fb5650cd1a879fe
Kelvaro <blizzardkang@gmail.com>
2016-01-10 00:55:11 -0800

    Added Display

--- index.php
+++ index.php
@@ -8,26 +8,23 @@
 
 
 
-$position = $_GET['board'];
-$squares = str_split($position);
+
+
 if (!isset($_GET['board']));
 
-$game = new Game($squares);
-if ($game.winner('x')) {
-    echo 'You win';
-} else if ($game.winner('o')) {
-    echo 'I win.';
-} else {
-    echo 'No Winnner yet';
-}
+
     
 
 class Game {
 
-    var $position;
+    var $position; //var to display the position
+    var $board; //var to display board
 
-    function __construct($squaures) {
-        $this->$position = str_split($squares);
+    function __construct($squares) {
+        
+        $this->position = $_GET['board'];
+        $squares = str_split($this->position);
+       
     }
 
     function winner($token) {
@@ -41,9 +38,9 @@ class Game {
                         ($this->position[4] == $token) &&
                         ($this->position[8] == $token)) { //checks diagonal top left to bottom right
                     $result = true;
-                } else if (($position[2] == $token) &&
-                        ($position[4] == $token) &&
-                        ($position[6] == $token)) { //checks diagonal top right to bottom left
+                } else if (($this->position[2] == $token) &&
+                        ($this->position[4] == $token) &&
+                        ($this->position[6] == $token)) { //checks diagonal top right to bottom left
                     $result = true;
                 }
             }
@@ -54,7 +51,7 @@ class Game {
             for ($col = 0; $col < 3; $col++) {//checks all columns
                 $result = true;
                 for ($row = 0; $row < 3; $row++) { //checks all rows
-                    if ($position[3 * $row + $col] != $token) {
+                    if ($this->position[3 * $row + $col] != $token) {
                         $result = false;
                     } else if (($position[0] == $token) && ($position[4] == $token) && ($position[8] == $token)) {//checks diagonal
                         $result = true;
@@ -64,7 +61,44 @@ class Game {
         }
         return $result;
     }
+    
+    function display() {
+        echo '<table cols="3" style ="font-size:large; font-weight:bold">';// style of the board
+        echo '<tr>';//open the first row
+        for ($pos=0; $pos<9; $pos++){
+            echo $this->show_cell($pos);
+            if ($pos %3 ==2) echo'</tr><tr>';//start a new row for the next square
+        }
+        echo '</tr>'; //close the last row
+        echo '</table>';
+        
+    function show_cell($which){
+        $token = $this->position[$which];
+        //deal with the easy case
+        if($token <> '-'){
+            return '<td>'.$token.'</td>';
+            //now the hard case
+            $this->newposition = $this->position;//copy the original
+            $this->newposition[$which] = 'o'; //this would be their move
+            $move = implode($this->newposition);// make a string from the board array
+            
+            $link = '/?board='.$move;//this is what we want the link to be
+            //so return a cell containing an anchor and showing a hyphen
+            return '<td><a=href="'.$link.'">-</a></td>';
+        }
+    }
+    }
  
+}
+
+$game = new Game("-");
+$game->display();
+if ($game->winner('x')) {
+    echo 'You win';
+} else if ($game->winner('o')) {
+    echo 'I win.';
+} else {
+    echo 'No Winnner yet';
 }
 
 
